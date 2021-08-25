558b109a1f4a7f0ba559b357ff776dc31bc251f2
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-08-25 08:27:01 -0500

    Delete 9-weather-app directory

--- 9-weather-app/src/apis/config.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = ['81eaae9c9ea6f28f239fe73eebafd259'];
-export const API_BASE_URL = 'http://api.openweathermap.org/';
--- 9-weather-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// bootstrap css
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// imports app and renders it at the root in index.html, which has the entire <body> tag w/ class of 'root'
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
